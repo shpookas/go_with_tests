225e5a4c28639b0dc2ec78e446e77eb7fb20cf9b
shpookas <vitas.spokas@protonmail.com>
2024-09-19 17:34:03 +0200

    more languages

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", " "))
 }
