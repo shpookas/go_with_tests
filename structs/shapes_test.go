b4f4074866f69ef6367268c6add45ef2135d0186
shpookas <vitas.spokas@protonmail.com>
2024-09-20 13:04:06 +0200

    interfaces, structs, methods

---
+++ structs/shapes_test.go
@@ -0,0 +1,72 @@
+package structs
+
+import (
+	"math"
+	"testing"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+func TestArea(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
+}
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
